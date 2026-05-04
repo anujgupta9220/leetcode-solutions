3928d2d3a0ef1e05cd51bf09f6af484b90e7e3b1
Anuj Prasad Gupta <anujgupta9220@gmail.com>
2026-05-04 16:23:21 +0530

    Implement matrix reshape functionality

---
+++ 0566-Reshape-the-Matrix/0566-Reshape-the-Matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m=mat.length;
+        int n=mat[0].length;
+        int matrix[][]=new int[r][c];
+        int row=0,col=0;
+        if(m*n==r*c){
+            for(int i=0;i<mat.length;i++){
+                for(int j=0;j<mat[0].length;j++){
+                    if(col==c){
+                        row++;
+                        col=0;
+                    }
+                    matrix[row][col]=mat[i][j];
+                    col++;
+                }
+            }
+        }else{
+            return mat;
+        }
+        return matrix;
+    }
+}
